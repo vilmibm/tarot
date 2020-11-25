050f861b0b397b245c5390f45b67317d8cea441c
vilmibm <vilmibm@github.com>
2020-11-24 21:25:43 -0800

    add rest of cards

--- cards.js
+++ cards.js
@@ -33,7 +33,7 @@ function cardDB() {
       "arcana": "major",
       "index": 8
     },
-    "justice": {
+    "strength": {
       "arcana": "major",
       "index": 9
     },
@@ -49,41 +49,45 @@ function cardDB() {
       "arcana": "major",
       "index": 12
     },
-    "death": {
+    "the hanged man": {
       "arcana": "major",
       "index": 13
     },
-    "temperance": {
+    "death": {
       "arcana": "major",
       "index": 14
     },
-    "the devil": {
+    "temperance": {
       "arcana": "major",
       "index": 15
     },
-    "the tower": {
+    "the devil": {
       "arcana": "major",
       "index": 16
     },
-    "the star": {
+    "the tower": {
       "arcana": "major",
       "index": 17
     },
-    "the moon": {
+    "the star": {
       "arcana": "major",
       "index": 18
     },
-    "the sun": {
+    "the moon": {
       "arcana": "major",
       "index": 19
     },
-    "judgement": {
+    "the sun": {
       "arcana": "major",
       "index": 20 
     },
-    "the world": {
+    "judgement": {
       "arcana": "major",
       "index": 21 
+    },
+    "the world": {
+      "arcana": "major",
+      "index": 22
     },
 
     // Minor arcana
@@ -92,8 +96,284 @@ function cardDB() {
       "index": 1,
       "suit": "cups"
     },
+    "ii of cups": {
+      "arcana": "minor",
+      "index": 2,
+      "suit": "cups"
+    },
+    "iii of cups": {
+      "arcana": "minor",
+      "index": 3,
+      "suit": "cups"
+    },
+    "iv of cups": {
+      "arcana": "minor",
+      "index": 4,
+      "suit": "cups"
+    },
+    "v of cups": {
+      "arcana": "minor",
+      "index": 5,
+      "suit": "cups"
+    },
+    "vi of cups": {
+      "arcana": "minor",
+      "index": 6,
+      "suit": "cups"
+    },
+    "vii of cups": {
+      "arcana": "minor",
+      "index": 7,
+      "suit": "cups"
+    },
+    "viii of cups": {
+      "arcana": "minor",
+      "index": 8,
+      "suit": "cups"
+    },
+    "ix of cups": {
+      "arcana": "minor",
+      "index": 9,
+      "suit": "cups"
+    },
+    "x of cups": {
+      "arcana": "minor",
+      "index": 10,
+      "suit": "cups"
+    },
+    "page of cups": {
+      "arcana": "minor",
+      "index": 11,
+      "suit": "cups"
+    },
+    "knight of cups": {
+      "arcana": "minor",
+      "index": 12,
+      "suit": "cups"
+    },
+    "queen of cups": {
+      "arcana": "minor",
+      "index": 13,
+      "suit": "cups"
+    },
+    "king of cups": {
+      "arcana": "minor",
+      "index": 14,
+      "suit": "cups"
+    },
 
-    // TODO
+    "ace of wands": {
+      "arcana": "minor",
+      "index": 1,
+      "suit": "wands"
+    },
+    "ii of wands": {
+      "arcana": "minor",
+      "index": 2,
+      "suit": "wands"
+    },
+    "iii of wands": {
+      "arcana": "minor",
+      "index": 3,
+      "suit": "wands"
+    },
+    "iv of wands": {
+      "arcana": "minor",
+      "index": 4,
+      "suit": "wands"
+    },
+    "v of wands": {
+      "arcana": "minor",
+      "index": 5,
+      "suit": "wands"
+    },
+    "vi of wands": {
+      "arcana": "minor",
+      "index": 6,
+      "suit": "wands"
+    },
+    "vii of wands": {
+      "arcana": "minor",
+      "index": 7,
+      "suit": "wands"
+    },
+    "viii of wands": {
+      "arcana": "minor",
+      "index": 8,
+      "suit": "wands"
+    },
+    "ix of wands": {
+      "arcana": "minor",
+      "index": 9,
+      "suit": "wands"
+    },
+    "x of wands": {
+      "arcana": "minor",
+      "index": 10,
+      "suit": "wands"
+    },
+    "page of wands": {
+      "arcana": "minor",
+      "index": 11,
+      "suit": "wands"
+    },
+    "knight of wands": {
+      "arcana": "minor",
+      "index": 12,
+      "suit": "wands"
+    },
+    "queen of wands": {
+      "arcana": "minor",
+      "index": 13,
+      "suit": "wands"
+    },
+    "king of wands": {
+      "arcana": "minor",
+      "index": 14,
+      "suit": "wands"
+    },
+
+    "ace of swords": {
+      "arcana": "minor",
+      "index": 1,
+      "suit": "swords"
+    },
+    "ii of swords": {
+      "arcana": "minor",
+      "index": 2,
+      "suit": "swords"
+    },
+    "iii of swords": {
+      "arcana": "minor",
+      "index": 3,
+      "suit": "swords"
+    },
+    "iv of swords": {
+      "arcana": "minor",
+      "index": 4,
+      "suit": "swords"
+    },
+    "v of swords": {
+      "arcana": "minor",
+      "index": 5,
+      "suit": "swords"
+    },
+    "vi of swords": {
+      "arcana": "minor",
+      "index": 6,
+      "suit": "swords"
+    },
+    "vii of swords": {
+      "arcana": "minor",
+      "index": 7,
+      "suit": "swords"
+    },
+    "viii of swords": {
+      "arcana": "minor",
+      "index": 8,
+      "suit": "swords"
+    },
+    "ix of swords": {
+      "arcana": "minor",
+      "index": 9,
+      "suit": "swords"
+    },
+    "x of swords": {
+      "arcana": "minor",
+      "index": 10,
+      "suit": "swords"
+    },
+    "page of swords": {
+      "arcana": "minor",
+      "index": 11,
+      "suit": "swords"
+    },
+    "knight of swords": {
+      "arcana": "minor",
+      "index": 12,
+      "suit": "swords"
+    },
+    "queen of swords": {
+      "arcana": "minor",
+      "index": 13,
+      "suit": "swords"
+    },
+    "king of swords": {
+      "arcana": "minor",
+      "index": 14,
+      "suit": "swords"
+    },
+
+    "ace of pentacles": {
+      "arcana": "minor",
+      "index": 1,
+      "suit": "pentacles"
+    },
+    "ii of pentacles": {
+      "arcana": "minor",
+      "index": 2,
+      "suit": "pentacles"
+    },
+    "iii of pentacles": {
+      "arcana": "minor",
+      "index": 3,
+      "suit": "pentacles"
+    },
+    "iv of pentacles": {
+      "arcana": "minor",
+      "index": 4,
+      "suit": "pentacles"
+    },
+    "v of pentacles": {
+      "arcana": "minor",
+      "index": 5,
+      "suit": "pentacles"
+    },
+    "vi of pentacles": {
+      "arcana": "minor",
+      "index": 6,
+      "suit": "pentacles"
+    },
+    "vii of pentacles": {
+      "arcana": "minor",
+      "index": 7,
+      "suit": "pentacles"
+    },
+    "viii of pentacles": {
+      "arcana": "minor",
+      "index": 8,
+      "suit": "pentacles"
+    },
+    "ix of pentacles": {
+      "arcana": "minor",
+      "index": 9,
+      "suit": "pentacles"
+    },
+    "x of pentacles": {
+      "arcana": "minor",
+      "index": 10,
+      "suit": "pentacles"
+    },
+    "page of pentacles": {
+      "arcana": "minor",
+      "index": 11,
+      "suit": "pentacles"
+    },
+    "knight of pentacles": {
+      "arcana": "minor",
+      "index": 12,
+      "suit": "pentacles"
+    },
+    "queen of pentacles": {
+      "arcana": "minor",
+      "index": 13,
+      "suit": "pentacles"
+    },
+    "king of pentacles": {
+      "arcana": "minor",
+      "index": 14,
+      "suit": "pentacles"
+    },
   }
 }
 
